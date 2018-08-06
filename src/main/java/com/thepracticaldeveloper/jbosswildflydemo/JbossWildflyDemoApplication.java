61823ab1aaa9c819d6490f0c7bc7320659e19656
Moises Macero <mechero@users.noreply.github.com>
2018-08-06 07:20:22 +0200

    Removed unnecessary configuration.

--- src/main/java/com/thepracticaldeveloper/jbosswildflydemo/JbossWildflyDemoApplication.java
+++ src/main/java/com/thepracticaldeveloper/jbosswildflydemo/JbossWildflyDemoApplication.java
@@ -2,10 +2,8 @@ package com.thepracticaldeveloper.jbosswildflydemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JbossWildflyDemoApplication extends SpringBootServletInitializer {
@@ -13,11 +11,6 @@ public class JbossWildflyDemoApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(JbossWildflyDemoApplication.class);
-    }
-
-    @Bean
-    public HttpMessageConverters httpMessageConverters() {
-        return new HttpMessageConverters();
     }
 
     public static void main(String[] args) {
